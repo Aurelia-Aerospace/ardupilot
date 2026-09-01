c0b9c6cec24a3821bace23365d03febbb3abbafb
4dri4anxD <mauricio.uavsystems@gmail.com>
2026-09-01 12:02:38 -0600

    AP_Notify: add flying_checks flag and corresponding tone alarm
    Adds flying_checks flag to AP_Notify flags struct and maps it to a
    distinct tone alarm pattern to alert the operator when flight is
    inhibited by the RemoteID system.

--- libraries/AP_Notify/AP_Notify.h
+++ libraries/AP_Notify/AP_Notify.h
@@ -139,6 +139,7 @@ public:
         bool video_recording;     // true when the vehicle is recording video
         bool temp_cal_running;    // true if a temperature calibration is running
         bool gyro_calibrated;     // true if calibrated gyro/acc
+        bool flying_checks;       // true if flying is not allowed
     };
 
     /// notify_events_type - bitmask of active events.
